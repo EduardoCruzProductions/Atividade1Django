224e5e8c8edaec1c9b21ab979f0c40586d137e57
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-03-29 20:47:44 -0300

    implementacao da pagina sobre

--- bootstrapDjangoSite/main/urls.py
+++ bootstrapDjangoSite/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from main.views import IndexView
+from main.views import SobreView
 
 urlpatterns = [
 	path('', IndexView.as_view(), name="index"),
+	path('sobre/', SobreView.as_view(), name="sobre"),
 ]
