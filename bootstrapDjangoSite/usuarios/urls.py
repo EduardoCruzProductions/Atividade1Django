5f110875d4b669e8cff00095450b8d8d45320d55
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-11-28 16:33:18 -0300

    implementacao de controle de usuarios

--- bootstrapDjangoSite/usuarios/urls.py
+++ bootstrapDjangoSite/usuarios/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import UsuarioCreate
+from django.urls import reverse_lazy
 
 urlpatterns = [
+
+    path('registrar/', UsuarioCreate.as_view(), name="user-create"),
+
     path('login/', auth_views.LoginView.as_view(
         template_name='usuarios/login.html',
         extra_context={'titulo': 'Autenticação'}
     ), name='login'),
+    
     path('sair/', auth_views.LogoutView.as_view(), name="logout"),
+
+    path('alterar-senha/', auth_views.PasswordChangeView.as_view(
+        template_name="usuarios/form.html",
+        extra_context={
+            'titulo': 'Alterar senha atual',
+            'botao': 'Alterar',
+            'classe': 'btn-success'
+        },
+        success_url=reverse_lazy('index')
+    ), name="alterar-senha")
+
 ]
--- bootstrapDjangoSite/usuarios/views.py
+++ bootstrapDjangoSite/usuarios/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class UsuarioCreate(CreateView):
+    form_class = UserCreationForm
+    template_name = "formulario.html"
+    success_url = reverse_lazy("login")
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UsuarioCreate, self).get_context_data(*args, **kwargs)
+        context['titulo'] = "Cadastro de novos Usuários"
+        context['botao'] = "Cadastrar"
+        context['classeBotao'] = "btn-success"
+        return context
+
+    def form_valid(self, form):
+
+        grupo = get_object_or_404(Group, name="Usuário")
+
+        url = super().form_valid(form)
+
+        if grupo:
+            self.object.groups.add(grupo)
+            self.object.save()
+
+        return url
